efe65717e1e5becf4538796d4036291c4a35fa0c
1804Op <b.burbut@optiflow-consulting.de>
2026-05-14 23:41:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function App() {
-  return (
+  return ( 
     <main style={styles.page}>
       <section style={styles.hero}>
         <div style={styles.overlay}>
